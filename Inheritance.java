0ce85089a85cdedcac9499a7c2af4839b496b5ea
Nithin U <106614289+NithinU2802@users.noreply.github.com>
2023-10-10 23:49:09 +0530

    Create Inheritance.java

---
+++ Inheritance.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.util.*;
+
+class Student{
+    static int student=0;
+     void add(){
+         student++;
+     }
+     void print(){
+         System.out.println(student+" ");
+     }
+}
+
+class Rockstar{
+    static int rockstar=0;
+     void add(){
+         rockstar++;
+     }
+     void print(){
+         System.out.println(rockstar+" ");
+     }
+}
+
+class Hacker{
+    static int hacker=0;
+     void add(){
+         hacker++;
+     }
+     void print(){
+         System.out.println(hacker+" ");
+     }
+}
+
+public class Solution {
+
+    public static void main(String[] args) {
+    Scanner x=new Scanner(System.in);
+    int n=x.nextInt();
+    x.nextLine();
+    String[] a=new String[n];
+    Student student=new Student();
+    Rockstar rockstar=new Rockstar();
+    Hacker hacker=new Hacker();
+    for(int i=0;i<n;i++){
+        a[i]=x.nextLine();
+        if(a[i].equals("Student"))
+            student.add();
+        else if(a[i].equals("Hacker"))
+            hacker.add();
+        else if(a[i].equals("Rockstar"))
+            rockstar.add();
+    }
+    student.print();
+    rockstar.print();
+    hacker.print();
+    }
+}
