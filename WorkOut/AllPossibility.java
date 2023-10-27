6867a09a660be44bcf3a53845f6f22c430c6d49f
Nithin U <106614289+NithinU2802@users.noreply.github.com>
2023-10-27 23:33:48 +0530

    Create AllPossibility.java

---
+++ WorkOut/AllPossibility.java
@@ -0,0 +1,16 @@
+// all possible arrangement of n 
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner x = new Scanner(System.in);
+        int n=x.nextInt();
+        int[] dp=new int[n+1];
+        dp[0]=1;
+        dp[1]=2;
+        for(int i=2;i<=n;i++)
+            dp[i]=dp[i-1]+dp[i-2];
+        System.out.print(dp[n]);
+    }
+}
