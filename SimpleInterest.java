ee88236ab0dc5cdf4b5645544f5aecb00ef11222
Nithin U <106614289+NithinU2802@users.noreply.github.com>
2024-02-14 22:53:05 +0530

    Create SimpleInterest.java

---
+++ SimpleInterest.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class SimpleInterest {
+	public static void main(String args[])
+	{
+		float P = x.nextFloat(), R = x.nextFloat(), T = x.nextFloat();
+		float SI = (P * T * R) / 100;
+		System.out.println("Simple interest: " + SI);
+	}
+}
