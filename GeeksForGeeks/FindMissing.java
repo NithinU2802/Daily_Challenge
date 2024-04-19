9000886bccadf9da4aa0714c03dcfe080477f23b
Nithin U <106614289+NithinU2802@users.noreply.github.com>
2024-04-19 10:42:26 +0530

    Create FindMissing.java

---
+++ GeeksForGeeks/FindMissing.java
@@ -0,0 +1,70 @@
+/******************************************************************************
+
+ Given two arrays a of size n and b of size m, the task is to find numbers which 
+ are present in the first array, but not present in the second array.
+
+Example 1:
+
+Input: 
+n = 6, m = 5
+a[] = {1, 2, 3, 4, 5, 10}
+b[] = {2, 3, 1, 0, 5}
+Output: 
+4 10
+Explanation: 
+4 and 10 are present in first array, but not in second array.
+Example 2:
+
+Input: 
+n = 5, m = 5
+a[] = {4, 3, 5, 9, 11}
+b[] = {4, 9, 3, 11, 10}
+Output: 
+5  
+Explanation: 
+Second array does not contain element 5.
+Your Task:
+This is a function problem. You don't need to take any input, as it is already 
+accomplished by the driver code. You just need to complete the function findMissing()
+that takes array a, array b, integer n, and integer m as parameters and returns an 
+array that contains the missing elements and the order of the elements should be the
+same as they are in array a.
+
+Expected Time Complexity: O(n+m).
+Expected Auxiliary Space: O(m).
+
+Constraints:
+1 ≤ n, m ≤ 105
+-109 ≤ A[i], B[i] ≤ 109
+
+*******************************************************************************/
+import java.util.*;
+
+public class FindMissing
+{
+    
+    static ArrayList<Integer> findMissing(int a[], int b[], int n, int m)
+    {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        ArrayList<Integer> res=new ArrayList<>();
+        for(int i=0;i<m;i++)
+            map.put(b[i],map.getOrDefault(b[i],1));
+        for(int i=0;i<n;i++)
+            if(!map.containsKey(a[i]))
+                res.add(a[i]);
+        return res;
+    }
+    
+	public static void main(String[] args) {
+		Scanner x=new Scanner(System.in);
+		int n=x.nextInt(),m=x.nextInt();
+		int[] a=new int[n];
+		int[] b=new int[m];
+		for(int i=0;i<n;i++)
+		    a[i]=x.nextInt();
+		for(int j=0;j<m;j++)
+		    b[j]=x.nextInt();
+		for(int i:findMissing(a,b,n,m))
+		    System.out.print(i+" ");
+	}
+}
