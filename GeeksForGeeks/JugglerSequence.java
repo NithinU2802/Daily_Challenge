91c1e52aaaf8d49492eacc286064fa085cda601e
Nithin U <106614289+NithinU2802@users.noreply.github.com>
2024-05-11 22:49:41 +0530

    Create JugglerSequence.java

---
+++ GeeksForGeeks/JugglerSequence.java
@@ -0,0 +1,70 @@
+/******************************************************************************
+
+Juggler Sequence is a series of integers in which the first term starts with a 
+positive integer number a and the remaining terms are generated from the immediate
+previous term using the below recurrence relation:
+
+Juggler Formula
+
+Given a number n, find the Juggler Sequence for this number as the first term of 
+the sequence until it becomes 1.
+
+
+Example 1:
+
+Input: n = 9
+Output: 9 27 140 11 36 6 2 1
+Explaination: We start with 9 and use 
+above formula to get next terms.
+ 
+
+Example 2:
+
+Input: n = 6
+Output: 6 2 1
+Explaination: 
+[61/2] = 2. 
+[21/2] = 1.
+ 
+
+Your Task:
+You do not need to read input or print anything. Your Task is to complete the function
+jugglerSequence() which takes n as the input parameter and returns a list of integers 
+denoting the generated sequence.
+
+ 
+
+Expected Time Complexity: O(n)
+Expected Auxiliary Space: O(n)
+
+ 
+
+Constraints:
+1 ≤ n ≤ 100
+
+*******************************************************************************/
+import java.util.*;
+
+public class JugglerSequence
+{
+    
+    static List<Long> jugglerSequence(long n) {
+        List<Long> res=new ArrayList<>();
+        while(true){
+            res.add(n);
+            if(n==1)
+                break;
+            if(n%2==0)
+                n=(long)Math.sqrt(n);
+            else
+                n=(long)(Math.sqrt(n)*Math.sqrt(n)*Math.sqrt(n));
+        }
+        return res;
+    }
+    
+	public static void main(String[] args) {
+		Scanner x=new Scanner(System.in);
+		long n=x.nextLong();
+		System.out.println(jugglerSequence(n));
+	}
+}
